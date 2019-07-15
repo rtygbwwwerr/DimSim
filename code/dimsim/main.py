c35c1dad7e86bd311ae96daecf5b6da419e755e0
taurus <rtygbwwwerr@163.com>
2019-07-16 00:10:06 +0800

    modified similarity

--- code/dimsim/main.py
+++ code/dimsim/main.py
@@ -22,19 +22,15 @@ def get_sentence_distance(s1, s2, max_loc_dis=10.0):
     phonetic_norm = lambda x, y: min(get_distance(x, y), max_loc_dis)
     #use DTW algorithm for computing sequence distance
     dist, cost_matrix, acc_cost_matrix, path = dtw(s1, s2, dist=phonetic_norm, warp=1)
+    
+    #normalize similarity with max distance
+    similarity = 1 - dist / (len(path[0]) * max_loc_dis)
     #normalize distance with minimum matching length
-#     match_score = 0
-#     norm_score = 0
-#     for i, j in zip(path[0], path[1]):
-#         norm_score += math.exp(cost_matrix[i, j])
-#         if cost_matrix[i, j] < max_loc_dis:
-#             match_score += math.exp(cost_matrix[i, j])
-#     smiliarty = match_score / norm_score
     dist = dist / (match_len * 2)
 #     plt.imshow(acc_cost_matrix.T, origin='lower', cmap='Blues', interpolation='nearest')
 #     plt.plot(path[0], path[1], 'w')
 #     plt.show()
-    return dist
+    return dist, similarity
 
 if __name__ == "__main__":
 #     dis = get_distance("较兰", "兰花")
@@ -42,8 +38,9 @@ if __name__ == "__main__":
     
 #     dis = get_sentence_distance("德华的忘情水", "忘情水")
 #     print(dis)
-    dis = get_sentence_distance("放歌较兰花啊", "兰花")
+    dis, similarity = get_sentence_distance("兰花", "兰花")
     print(dis)
+    print(similarity)
 #     dis = get_sentence_distance("兰花", "兰色的花")
 #     print(dis)
 #     dis = get_sentence_distance("兰花", "兰色花")
